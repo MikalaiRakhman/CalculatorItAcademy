d3d598a506b7bf867dca4b9d37fc963083e87394
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-08-18 22:45:41 +0200

    Добавьте файлы проекта.

---
+++ CalculatorTest/CalculatorTests.cs
@@ -0,0 +1,42 @@
+using Calculator;
+
+namespace CalculatorTest
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator.Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator.Calculator();
+        }
+
+        [Fact]
+        public void SumShouldAddTwoPositiveNumbersAndGetPositiveResult_PositiveTest()
+        {
+            // Arrange
+            int a = 5;
+            int b = 3;
+
+            // Act
+            int result = _calculator.Sum(a, b);
+
+            // Assert
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public void SumShouldAddTwoPositiveNumbersAndGetPositiveResult_NegativeTest()
+        {
+            // Arrange
+            int a = Int32.MaxValue;
+            int b = 3;
+
+            // Act
+            int result = _calculator.Sum(a, b);
+
+            // Assert
+            Assert.False(result > int.MaxValue);
+        }
+    }
+}
